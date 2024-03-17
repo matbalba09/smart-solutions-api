4ddd57c436bd3c27bb07b287bd224865935d5345
DESKTOP-9DIS1O6\Reiy <mat.balba09@gmail.com>
2024-03-17 16:31:39 +1100

    add to user and eventUser seeder and create new log seeder

--- database/seeders/LogSeeder.php
+++ database/seeders/LogSeeder.php
@@ -3,21 +3,27 @@
 namespace Database\Seeders;
 
 use App\Helper\Helper;
-use App\Models\EventUser;
+use App\Models\Log;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class EventUserSeeder extends Seeder
+class LogSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        EventUser::truncate();
-        EventUser::insert([
+        Log::truncate();
+        Log::insert([
             [
                 'event_id' => 1,
+                'user_id' => 1,
+                'created_at' => Helper::getDateNow(),
+                'updated_at' => Helper::getDateNow(),
+            ],
+            [
+                'event_id' => 3,
                 'user_id' => 1,
                 'created_at' => Helper::getDateNow(),
                 'updated_at' => Helper::getDateNow(),
@@ -29,26 +35,8 @@ class EventUserSeeder extends Seeder
                 'updated_at' => Helper::getDateNow(),
             ],
             [
-                'event_id' => 1,
-                'user_id' => 3,
-                'created_at' => Helper::getDateNow(),
-                'updated_at' => Helper::getDateNow(),
-            ],
-            [
-                'event_id' => 3,
-                'user_id' => 1,
-                'created_at' => Helper::getDateNow(),
-                'updated_at' => Helper::getDateNow(),
-            ],
-            [
                 'event_id' => 3,
                 'user_id' => 2,
-                'created_at' => Helper::getDateNow(),
-                'updated_at' => Helper::getDateNow(),
-            ],
-            [
-                'event_id' => 3,
-                'user_id' => 3,
                 'created_at' => Helper::getDateNow(),
                 'updated_at' => Helper::getDateNow(),
             ],

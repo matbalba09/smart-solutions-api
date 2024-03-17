98cd3430488c646ca9402523dbccc2b9bc95bec1
DESKTOP-9DIS1O6\Reiy <mat.balba09@gmail.com>
2024-03-17 17:16:02 +1100

    add LogSeeder call to DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             EventSeeder::class,
             EventUserSeeder::class,
+            LogSeeder::class,
         ]);
     }
 }
